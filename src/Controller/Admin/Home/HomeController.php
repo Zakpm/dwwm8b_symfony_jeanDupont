58c8b8ab770e6ab597f675fda1e466b9e291d1e7
Zakaryya  PAPA MOHAMED <zakaryya@macbook-pro-de-zakaryya.home>
2022-12-21 17:05:13 +0100

    Mise en place de la page Admin

--- src/Controller/Admin/Home/HomeController.php
+++ src/Controller/Admin/Home/HomeController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Admin\Home;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 class HomeController extends AbstractController
 {
-    #[Route('/admin/home/home', name: 'admin.home.index')]
+    #[Route('/admin/home', name: 'admin.home.index')]
     public function index(): Response
     {
         return $this->render('pages/admin/home/index.html.twig' );
